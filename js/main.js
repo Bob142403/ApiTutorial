b3fc612ba61501cc701d2939302b21d0ef383280
Bob142403 <boboshka2006@mail.ru>
2023-01-11 14:36:03 +0300

    Bob

--- js/main.js
+++ js/main.js
@@ -1,29 +1,44 @@
 let url = "https://jsonplaceholder.typicode.com/posts";
 let securl = "https://jsonplaceholder.typicode.com/users";
+let userInfo = document.getElementById("userInfo");
+let emailInfo = document.getElementById("emailInfo");
+let website = document.getElementById("website");
+let titlemodal = document.querySelector(".modal-title");
 let username = [];
 
-async function fetchs () {
+async function fetchs() {
   let response = await fetch(securl);
 
   let commits = await response.json();
 
   for (let value of commits) {
-    username[value.id] = await value.name;
+    username[value.id] = value;
   }
 
   response = await fetch(url);
-  
+
   commits = await response.json();
 
   for (let value of commits) {
     let card = tmpl.content.cloneNode(true);
     let title = card.querySelector(".card-title");
     let body = card.querySelector(".card-text");
-    let userName = card.querySelector(".fs-4");
+    let userName = card.querySelector("p");
+    let info = username[value.userId];
 
     title.innerHTML = value.title;
     body.innerHTML = value.body;
-    userName.innerHTML += username[value.userId];
+    userName.innerHTML += info.name;
+
+    userName.setAttribute("data-bs-target", "#staticBackdrop");
+    userName.dataset.bsToggle = "modal";
+
+    userName.addEventListener("click", () => {
+      titlemodal.innerHTML = `About ${info.name}`;
+      userInfo.innerHTML = info.username;
+      emailInfo.innerHTML = info.email;
+      website.innerHTML = info.website;
+    });
 
     list.append(card);
   }
