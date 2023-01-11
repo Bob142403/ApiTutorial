9b11b403a26d3a217b897fb007aadf8df16a17a4
Bob142403 <boboshka2006@mail.ru>
2023-01-11 11:45:44 +0300

    tets

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 let url = "https://jsonplaceholder.typicode.com/posts";
+let securl = "https://jsonplaceholder.typicode.com/users";
+let username = [];
+
+fetch(securl)
+  .then((response) => response.json())
+  .then((commits) => {
+    for (let value of commits) {
+      console.log(value.id);
+      username[value.id] = value.name;
+    }
+  });
 fetch(url)
   .then((response) => response.json())
   .then((commits) => {
@@ -6,10 +17,12 @@ fetch(url)
       let card = tmpl.content.cloneNode(true);
       let title = card.querySelector(".card-title");
       let body = card.querySelector(".card-text");
-      
+      let userName = card.querySelector(".fs-4");
+
       title.innerHTML = value.title;
       body.innerHTML = value.body;
+      userName.innerHTML += username[value.userId];
 
-      list.append(card);
+      if (value.userId == 1) list.append(card);
     }
   });
